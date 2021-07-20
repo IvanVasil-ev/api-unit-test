bac1ca389983540d115abcc090615b4a6d507c17
Ivan <i.vasilev@dunice.net>
2021-07-20 17:42:15 +0300

    Added cors and device config

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
                  sessions: 'users/sessions',
                  registrations: 'users/registrations'
              }
+  post '/authentication_tokens', to: "authentication_tokens#create"
   get '/member-data', to: 'members#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
